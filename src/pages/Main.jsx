a3ecc39f4406a82a9f7ba27f68f07f32465aaca1
denis-rossati <denis.rossati@hotmail.com>
2021-08-23 19:10:52 -0300

    fix query in the tests

--- src/pages/Main.jsx
+++ src/pages/Main.jsx
@@ -1,11 +1,12 @@
 import React from 'react';
 import ButtonNext from '../components/ButtonNext';
 
+// I do not like to use data-testid, but I had no chance :v
 export default function main() {
   return (
     <main>
       <div>
-        <p id="greeting">
+        <p id="greeting" data-testid="greeting-element">
           Hi, I&apos;m
           <span id="my-name"> Denis</span>
         </p>
